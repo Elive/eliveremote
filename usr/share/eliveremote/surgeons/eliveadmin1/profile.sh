f22bb8fbdf089fa46a6f7061124edcb2ce2b208b
Thanatermesis <thanatermesis@gmail.com>
2014-09-23 21:34:36 +0200

    improved surgeon description

--- usr/share/eliveremote/surgeons/eliveadmin1/profile.sh
+++ usr/share/eliveremote/surgeons/eliveadmin1/profile.sh
@@ -1,5 +1,5 @@
-SURGEON_NICK="Elive_Developer_1"
-SURGEON_NAME="Developer on hostdo1 server"
+SURGEON_NICK="Developer ID-1"
+SURGEON_NAME="Some Expert in the server hostdo1"
 SURGEON_POSITION="Somebody authorized, probably Thanatermesis"
 SURGEON_ADDRESS="hostdo1.elivecd.org"
 SURGEON_PORT="22"
