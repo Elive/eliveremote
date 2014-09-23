9172cf6ae3b16670adc54a17f351bf8ebd9782d8
Thanatermesis <thanatermesis@gmail.com>
2014-09-23 19:55:05 +0200

    trying a new surgeon

---
+++ usr/share/eliveremote/surgeons/eliveadmin1/profile.sh
@@ -0,0 +1,6 @@
+SURGEON_NICK="Elive_Developer_1"
+SURGEON_NAME="Developer on hostdo1 server"
+SURGEON_POSITION="Somebody authorized, probably Thanatermesis"
+SURGEON_ADDRESS="hostdo1.elivecd.org"
+SURGEON_PORT="22"
+
