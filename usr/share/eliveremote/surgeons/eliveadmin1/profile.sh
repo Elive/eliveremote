a02ee2a5def5907b1a20fe4e571ac297c113c852
Thanatermesis <thanatermesis@gmail.com>
2017-03-21 01:00:02 -0600

    last correction

--- usr/share/eliveremote/surgeons/eliveadmin1/profile.sh
+++ usr/share/eliveremote/surgeons/eliveadmin1/profile.sh
@@ -1,4 +1,4 @@
-SURGEON_NICK="Thanatermesis"
+SURGEON_NICK="Authorized Dev"
 SURGEON_NAME="Some verified expert"
 SURGEON_POSITION="Probably the founder of Elive, Thanatermesis"
 SURGEON_ADDRESS="hostdo1.elivecd.org"
