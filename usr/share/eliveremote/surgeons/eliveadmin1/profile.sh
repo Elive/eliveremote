5dd49e717e57b0815536942aed654340b0060194
Thanatermesis <thanatermesis@gmail.com>
2017-03-21 00:01:00 -0600

    fixes

--- usr/share/eliveremote/surgeons/eliveadmin1/profile.sh
+++ usr/share/eliveremote/surgeons/eliveadmin1/profile.sh
@@ -1,5 +1,5 @@
 SURGEON_NICK="Developer ID-1"
-SURGEON_NAME="Some Expert in the server hostdo1"
+SURGEON_NAME="Some verified expert"
 SURGEON_POSITION="Somebody authorized, probably Thanatermesis"
 SURGEON_ADDRESS="hostdo1.elivecd.org"
 SURGEON_PORT="22"
