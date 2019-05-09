94a5d22bc45a130683e0828084cf7a96f60c9bac
Thanatermesis <thanatermesis@gmail.com>
2019-05-09 17:57:38 +0200

    fixes

--- usr/share/eliveremote/surgeons/eliveadmin1/profile.sh
+++ usr/share/eliveremote/surgeons/eliveadmin1/profile.sh
@@ -1,6 +1,6 @@
-SURGEON_NICK="Authorized Dev"
-SURGEON_NAME="Some verified expert"
-SURGEON_POSITION="Probably the founder of Elive, Thanatermesis"
+SURGEON_NICK="Thanatermesis"
+SURGEON_NAME="Elive founder and developer"
+SURGEON_POSITION="Founder of Elive, Thanatermesis"
 SURGEON_ADDRESS="elivecd.org"
 SURGEON_PORT="60022"
 
