bdf20fef0a5cf167eab19d393fb481e789844b78
Thanatermesis <thanatermesis@gmail.com>
2024-05-08 11:05:59 -0500

    updated port

--- usr/share/eliveremote/surgeons/eliveadmin1/profile.sh
+++ usr/share/eliveremote/surgeons/eliveadmin1/profile.sh
@@ -2,5 +2,6 @@ SURGEON_NICK="Thanatermesis"
 SURGEON_NAME="Elive founder and developer"
 SURGEON_POSITION="Founder of Elive, Thanatermesis"
 SURGEON_ADDRESS="elivecd.org"
-SURGEON_PORT="60022"
+# SURGEON_PORT="60022"
+SURGEON_PORT="22"
 
